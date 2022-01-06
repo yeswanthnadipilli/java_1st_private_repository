f9179806f54dca365e74d8cfe4a25ad22dc99b75
yeswanth.naidu <yeswanth.naidu@collabera.com>
2022-01-06 20:33:34 +0530

    javabasicvariables

---
+++ week1/day2/src/main/java/JavaBasics.java
@@ -0,0 +1,15 @@
+public class JavaBasics {
+
+    public void testVariables()
+    {
+        int a=10;
+        float b=23.2f;
+        char ch='a';
+        double d=23.34;
+        byte by=2;
+        short s=13;
+        long ln=135555L;
+        String str="yeswanth";
+
+    }
+}
