e4df979b4bc0c5b65c9a900cb91a2b20d2fac177
yeswanth.naidu <yeswanth.naidu@collabera.com>
2022-01-07 17:23:07 +0530

    spring simple app

---
+++ week1/day5/spring-basic-app/src/main/java/car/Main.java
@@ -0,0 +1,19 @@
+package car;
+import car.Car;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+public class Main {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+        Car car1 = (Car) context.getBean("carak");
+        car1.setSpeed(100);
+
+        Class<Car> cls = Car.class; // this is not the bytecode class file
+        Car car2 = context.getBean(cls);
+        System.out.println("Car Speed is "+ car2.getSpeed());
+
+    }
+}
