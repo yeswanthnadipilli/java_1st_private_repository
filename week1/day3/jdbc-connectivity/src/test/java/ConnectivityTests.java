949407b7e5450eebb2ec5ab8dd26f83e204846ad
yeswanth.naidu <yeswanth.naidu@collabera.com>
2022-01-08 14:14:10 +0530

    jdbc connection

---
+++ week1/day3/jdbc-connectivity/src/test/java/ConnectivityTests.java
@@ -0,0 +1,17 @@
+import db.Connectivity;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ConnectivityTests {
+    @DisplayName("Testing database Connection")
+    @Test
+    void testConnection() {
+        Assertions.assertDoesNotThrow(() -> {
+            Connectivity con = new Connectivity();
+            Assertions.assertNotNull(con.getConnection());
+
+        });
+
+    }
+}
