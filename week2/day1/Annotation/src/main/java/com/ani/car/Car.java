6f11ed9a1e6a464df9733b4f37ee7016f1576e1f
yeswanth.naidu <yeswanth.naidu@collabera.com>
2022-01-10 11:59:30 +0530

    spring core : annotation based configuration

--- week2/day1/Annotation/src/main/java/com/ani/car/Car.java
+++ week2/day1/Annotation/src/main/java/com/ani/car/Car.java
@@ -1,4 +1,11 @@
 package com.ani.car;
 
+import com.ani.tempo.Tempo;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Car {
+
+    @Autowired
+    private Tempo tempo;
+
 }
---
+++ week2/day1/Annotation/src/main/java/com/ani/tempo/Tempo.java
@@ -0,0 +1,10 @@
+package com.ani.tempo;
+
+
+import org.springframework.stereotype.Component;
+
+@Component  //@Repository, @Service, @Controller
+public class Tempo {
+
+
+}
