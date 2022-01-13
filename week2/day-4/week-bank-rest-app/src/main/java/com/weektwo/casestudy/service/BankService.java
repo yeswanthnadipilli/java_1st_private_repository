59eb763872e080bc35bba781f834c93c61075bb9
yeswanth.naidu <yeswanth.naidu@collabera.com>
2022-01-14 00:52:48 +0530

    spring boot : using jpgnterface

---
+++ week2/day-4/week-bank-rest-app/src/main/java/com/weektwo/casestudy/service/BankService.java
@@ -0,0 +1,32 @@
+package com.weektwo.casestudy.service;
+
+import com.weektwo.casestudy.domain.BankAccount;
+
+import java.util.List;
+
+public interface BankService {
+
+    void createNewAccount(BankAccount ba);
+
+    int updateAccountDetails(BankAccount ba);
+
+    boolean activateAccount(Long acNum);
+
+    boolean deactivateAccount(Long acNum);
+
+    double withdraw(Long acNum, double amt);
+
+
+    double deposit(Long acNum, double amt);
+
+    int transferMoney(Long srcAc, Long dstAc, double amt);
+
+    BankAccount findAccountByAcNum(Long acNum);
+
+    List<BankAccount> findAllBankAccounts();
+
+
+
+
+    List<BankAccount> namesStartsWith(String prefix);
+}
