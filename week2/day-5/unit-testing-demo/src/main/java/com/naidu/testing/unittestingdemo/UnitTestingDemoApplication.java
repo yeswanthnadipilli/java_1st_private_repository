d7254773a3751e9ab9127997def406f95a609e15
yeswanth.naidu <yeswanth.naidu@collabera.com>
2022-01-14 10:55:43 +0530

    rest using unit testcases

---
+++ week2/day-5/unit-testing-demo/src/main/java/com/naidu/testing/unittestingdemo/UnitTestingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.naidu.testing.unittestingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitTestingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitTestingDemoApplication.class, args);
+	}
+
+}
---
+++ week2/day-5/unit-testing-demo/src/main/java/com/naidu/testing/unittestingdemo/controller/CarController.java
@@ -0,0 +1,32 @@
+package com.naidu.testing.unittestingdemo.controller;
+
+
+import com.naidu.testing.unittestingdemo.domine.Car;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.Date;
+import java.time.LocalDate;
+
+
+@RequestMapping("/cra")
+@RestController
+public class CarController {
+
+
+    @GetMapping
+    public ResponseEntity<Car> getCar() {
+        var car = new Car();
+        car.setCost(56.23);
+        car.setId(10L);
+        car.setModel("abc");
+        car.setMfg(Date.valueOf(LocalDate.now()));
+        return ResponseEntity.ok(car);
+    }
+
+    @PostMapping
+    public ResponseEntity<Car> saveCar(@RequestBody Car car) {
+        return new ResponseEntity<>(car, HttpStatus.CREATED);
+    }
+}
