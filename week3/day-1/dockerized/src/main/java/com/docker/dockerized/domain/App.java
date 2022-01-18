361e9b7e30a2f569b6167656be441c9f6d001ebf
yeswanth.naidu <yeswanth.naidu@collabera.com>
2022-01-18 15:57:02 +0530

    docker

---
+++ week3/day-1/dockerized/src/main/java/com/docker/dockerized/domain/App.java
@@ -0,0 +1,20 @@
+package com.docker.dockerized.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+
+
+@AllArgsConstructor
+@Data
+public class App {
+
+
+    private long id;
+    private int version;
+    private String name;
+    private LocalDate publishedDate;
+    private boolean isFree;
+}
---
+++ week3/day-1/dockerized/src/test/java/com/docker/dockerized/DockerizedApplicationTests.java
@@ -0,0 +1,23 @@
+package com.docker.dockerized;
+
+import com.docker.dockerized.controller.AppController;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerizedApplicationTests {
+
+
+	@Autowired
+	private AppController controller;
+
+	@DisplayName("Context : Spring Application Context Loaded")
+	@Test
+	void contextLoads() {
+		Assertions.assertNotNull(controller);
+	}
+
+}
